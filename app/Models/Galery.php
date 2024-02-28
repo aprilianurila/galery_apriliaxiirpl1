2830a72a32e7eb12fd5ee89794ef551370d0c4fb
aprilianurila <aprilianurila06@gmail.com>
2024-02-28 14:46:09 +0700

    pertama_upload

---
+++ app/Models/Galery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
+
+class Galery extends Model
+{
+    use HasFactory;
+    use softDeletes;
+    protected $fillable =[
+        'judul',
+        'deskripsi',
+        'tanggal',
+        'foto',
+        'iduser',
+    ];
+}
